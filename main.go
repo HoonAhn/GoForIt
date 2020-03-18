77fe83d2c8536807308587e5b112a6554d3cd4b2
Hoon Ahn <shahn801@gmail.com>
2020-03-18 23:20:02 +0900

    practicing go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
 
 func main() {
-  
+	fav := []string{"kimchi", "real ramen"}
+	hoon := person{name: "bacon", favFood: fav, age: 20}
+	fmt.Println(hoon)
 }
