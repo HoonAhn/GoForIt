d65e55cec7b50e350b467b6ad8a8c17bca6037be
Hoon Ahn <shahn801@gmail.com>
2020-03-20 20:38:38 +0900

    fast url checker using goroutine

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request Failed!")
 
 func main() {
+	ch := make(chan requestResult)
 	results := map[string]string{}
 	urls := []string{
 		"https://www.google.com/",
@@ -20,25 +26,22 @@ func main() {
 		"https://soundcloud.com/",
 	}
 	for _, url := range urls {
-		resultStatus := "OK"
-		err := hitURL(url)
-		if err != nil {
-			resultStatus = "FAILED"
-		}
-		results[url] = resultStatus
+		go hitURL(url, ch)
 	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(urls); i++ {
+		result := <-ch
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
+func hitURL(url string, ch chan<- requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FALILED"
 	}
-	fmt.Println(err)
-	return nil
+	ch <- requestResult{url: url, status: status}
 }
