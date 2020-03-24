747628cf75a813d2426dc3d10ab687cf84b6cc13
Hoon Ahn <shahn801@gmail.com>
2020-03-24 17:44:39 +0900

    app workes without goroutine and makes .csv file

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -21,27 +23,51 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
+	var jobs []extractedJob
 	pages := getPages()
 	for i := 0; i < pages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
+	}
+	writeJobs(jobs)
+	fmt.Println("Done, jobs extracted. ", len(jobs))
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
 	}
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
 	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting... ", pageURL)
 	res, err := http.Get(pageURL)
 	checkErr(err)
 	checkStatusCode(res)
-	// jobsearch-SerpJobCard
 	defer res.Body.Close()
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
-	searchCards := doc.Find(".jobsearch-SerpJobCard").Each(func(i int, card *goquery.Selection) {
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
 		job := extractJob(card)
 		jobs = append(jobs, job)
 	})
-	fmt.Println(searchCards)
+	return jobs
 }
 
 func extractJob(card *goquery.Selection) extractedJob {
